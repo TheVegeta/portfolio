50422da68b1d82a984fc3e1d67037cd20bb2bd9c
TheVegeta <iamvegeta@protonmail.com>
2023-09-10 21:07:41 +0530

    added vite build step for github pages

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,7 @@ import { defineConfig } from "vite";
 export default defineConfig({
   plugins: [react()],
   json: { stringify: true },
+  base: "/portfolio-website/",
   build: {
     rollupOptions: {
       output: {
